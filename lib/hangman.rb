98af541d1eb6e506098f7456ad3999cd574eb0cc
rachuses <133572545+rachuses1@users.noreply.github.com>
2024-08-23 18:03:20 +1000

    moved methods into a class

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,54 +1,67 @@
-list = File.read('google-10000-english-no-swears.txt').split("\n")
+require "yaml"
+
+display = []
+incorrect = []
 dictionary = []
 
+list = File.read('google-10000-english-no-swears.txt').split("\n")
 list.each do |word|
   if word.length >= 5 && word.length <= 12
     dictionary.push(word)
   end
-end # generate list of words between 5 and 12 in length.
+end 
+# generate list of words between 5 and 12 in length.
 
 secret_word = dictionary[rand(dictionary.length - 1)]
+# randomly select a word between 5 and 12 in length.
 
-display = []
-secret_word.length.times {|i| display.push('_ ')}
-incorrect = []
-
-
-def progress(secret_word, display, incorrect)
+class Play
   
-  
-  puts secret_word
-  puts display.join
-  puts "Incorrect guesses: #{incorrect}"
-  
-  guess = gets.chomp
+  def initialize(secret_word, incorrect, display)
+    @secret_word = secret_word
+    @incorrect = incorrect
+    @display = display
+  end
 
-  secret_array = secret_word.split("")
+  def start()
+    @secret_word.length.times {|i| @display.push('_ ')}
+    puts @display.join
+    progress()
+  end
 
-  if secret_word.include?(guess)
-    secret_array.each_index{|index|
-      if secret_array[index] == guess
-        display[index] = "#{guess} "
+  def progress()
+    puts @secret_word
+    puts "Incorrect guesses: #{@incorrect}"
+  
+    guess = gets.chomp
+
+    secret_array = @secret_word.split("")
+
+    if @secret_word.include?(guess)
+      secret_array.each_index{|index|
+        if secret_array[index] == guess
+          @display[index] = "#{guess} "
+        end
+      }
+      if @display.join.gsub(" ", "") == @secret_word 
+        puts @display.join
+        puts "Yay! You have guessed the word!"
+      else
+        puts @display.join
+        progress()
       end
-    }
-    if display.join.gsub(" ", "") == secret_word 
-      puts display.join
-      puts "Yay! You have guessed the word!"
+      
     else
-      puts display.join
-      progress(secret_word, display, incorrect)
-    end
-    
-  else
-    incorrect << guess
-    if incorrect.length == 7
-      puts "Oh no, you're out of guesses!"
-    else
-    progress(secret_word, display, incorrect)
+      @incorrect << guess
+      if @incorrect.length == 7
+        puts "Oh no, you're out of guesses!"
+      else
+      progress()
+      end
     end
   end
 
-
 end
 
-progress(secret_word, display, incorrect)
+show = Play.new(secret_word, incorrect, display)
+show.start()
